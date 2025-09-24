facdb4d53d9b9aceaab9a5e4b5bc5c05a76a8f7c
Frida Jansson <frja2400@student.miun.se>
2025-09-24 13:06:28 +0200

    Skapade en klass Guestbook som innehåller en privat lista av GuestbookPost-objekt, samt metoder för att lägga till nya inlägg (AddPost) och skriva ut alla inlägg (ShowAllPosts). Programmet körs genom att skapa nya GuestbookPost-objekt, lägga till dem i gästboken och visa dem.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;   //Importerar system som t ex Console.WriteLine
+﻿using System;
+using System.Dynamic;   //Importerar system som t ex Console.WriteLine
 
 //Skapar en klass för gästbokinlägg
 class GuestbookPost
@@ -44,28 +45,58 @@ class GuestbookPost
     }
 }
 
+//Skapar en klass för gästboken
+class Guestbook
+{
+    //Skapar en lista med namnet posts som kan lagra GuestbookPost-objekt. Gör den privat så att jag kan styra reglerna.
+    private List<GuestbookPost> posts = new List<GuestbookPost>();
+
+    //Skapar en metod som skriver ut alla inlägg.
+    public void ShowAllPosts()
+    {
+        //Kontrollerar om det finns några inlägg. Är den tom så skriv ut det och avbryt metoden.
+        if (posts.Count == 0)
+        {
+            Console.WriteLine("Gästboken är tom.");
+            return;
+        }
+
+        //Loopar igenom alla inlägg och skriver ut alla inlägg med index.
+        for (int i = 0; i < posts.Count; i++)
+        {
+            Console.WriteLine($"{i} {posts[i].Owner} - {posts[i].Text}");
+        }
+    }
+
+    //Skapar en metod som lägger till ett nytt inlägg i gästboken
+    //Metoden behöver ta emot ett inlägg som parameter (pga kallar den i Main gb.AddPost(post1)).
+    public void AddPost(GuestbookPost post)     //Parameter post av typen GuestbookPost.
+    {
+        //Lägg till i listan posts med inbyggd metod för listor Add()
+        posts.Add(post);
+    }
+
+}
+
 
 //Kör programmet
 class Program
 {
     static void Main()
     {
-        //Skapa ett nytt inlägg (objekt) av klassen GuestbookPost
+        //Skapa gästboken som inläggen ska adderas till. En instans av Guestbook.
+        Guestbook gb = new Guestbook();
+
+        //Skapa nya inlägg (objekt) av klassen GuestbookPost. Anropar konstruktorn i GuestbookPost som kör koden för att skapa ett nytt objekt.
         GuestbookPost post1 = new GuestbookPost("Frida", "Hello World!");
+        GuestbookPost post2 = new GuestbookPost("Hanna", "Testmeddelande.");
 
-        //Skriv ut inlägg
-        Console.WriteLine("Ägare: " + post1.Owner);
-        Console.WriteLine("Inlägg: " + post1.Text);
+        //Addera inlägg i gästboken med addera-metoden.
+        gb.AddPost(post1);
+        gb.AddPost(post2);
 
-        //Testa ett fel med try-catch (gör det mer användarvänligt när användaren kan skriva fel)
-        try
-        {
-            GuestbookPost post2 = new GuestbookPost("", "Hello World!");
-        }
-        //Fel vi vill fånga och variabel(ex) som refererar till felobjektet.
-        catch (ArgumentException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
+        //Skriv ut inläggen med metoden för utskrift
+        gb.ShowAllPosts();
+       
     }
 }
